8173422d7a043cbbd8ca603ca35cd77587224468
StigSH <33154983+StigSH@users.noreply.github.com>
2018-02-09 13:16:57 +0100

    Update RODBC.c

--- src/RODBC.c
+++ src/RODBC.c
@@ -736,7 +736,9 @@ static int cachenbind(pRODBCHandle thisHandle, int nRows)
 	default:
 	{
 	    SQLLEN datalen = thisHandle->ColData[i].ColSize;
-	    /*if (datalen <= 0 || datalen < COLMAX) datalen = COLMAX;*/
+	    Rprintf(datalen)
+	    
+	    if (datalen <= 0 || datalen < COLMAX) datalen = COLMAX;
 	    /* sanity check as the reports are sometimes unreliable */
 	    /*if (datalen > 65535) datalen = 65535;*/
 	    thisHandle->ColData[i].pData =
